dc776ae535a17d76fd6734ba7266ed33e1dfe74c
Dynami27 <Neuron@Khalids-MacBook-Pro.local>
2016-11-01 10:04:17 -0500

    Pokemon API get request
    API get request.

---
+++ CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Pokemon
+//
+//  Created by Khalid Mohamed on 10/31/16.
+//  Copyright © 2016 Khalid Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+   @IBOutlet  weak var imageView :UIImageView?
+   @IBOutlet weak var textLabel :UILabel?
+    
+}
